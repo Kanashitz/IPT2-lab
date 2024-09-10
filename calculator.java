c0b1adedf371050342467867b1d011807231d66b
Mendoza-Cabading-Capan <kevsmendoza2503@gmail.com>
2024-09-10 10:46:33 +0800

    subtraction

--- calculator.java
+++ calculator.java
@@ -1,11 +1,14 @@
 public class calculator{
     public static int add(int a, int b){
     return a + b;
-
     }
 
+    public static int subtract(int a, int b){
+        return a - b;
+        }
 
     public static void main (String[] args){
         System.out.println("Sum: " + add(20,10));
+        System.out.println("Difference: " + subtract(20,5));
     }
-}
+    }
