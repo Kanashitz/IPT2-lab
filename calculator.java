6a9e0fe8bd8b5c126145de87879facca899ffcea
Mendoza-Cabading-Capan <kevsmendoza2503@gmail.com>
2024-09-10 10:35:14 +0800

    addition

---
+++ calculator.java
@@ -0,0 +1,11 @@
+public class calculator{
+    public static int add(int a, int b){
+    return a + b;
+
+    }
+
+
+    public static void main (String[] args){
+        System.out.println("Sum: " + add(20,10));
+    }
+}
