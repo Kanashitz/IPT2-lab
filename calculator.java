37c8ab8965f66391c53b856d0136cb19783c47ef
Mendoza-Cabading-Capan <kevsmendoza2503@gmail.com>
2024-09-10 11:00:18 +0800

    division

--- calculator.java
+++ calculator.java
@@ -10,9 +10,18 @@ public class calculator{
     public static int multiply(int a, int b){
             return a * b;
             }
+    public static int divide(int a , int b){
+            if (b==0){
+                throw new
+                ArithmeticException("Division by zero is not allowed");
+            }
+                return a/b;
+            }
+    
     public static void main (String[] args){
         System.out.println("Sum: " + add(20,10));
         System.out.println("Difference: " + subtract(20,5));
         System.out.println("Product: " + multiply(20,5));
+        System.out.println("Quotient: " + divide(50,5));
     }
-    }
+}
